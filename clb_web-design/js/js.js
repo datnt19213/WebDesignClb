a7f087b8a5dcc7c6ef9a01ca671e245e542b51f3
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2022-11-29 12:20:41 +0700

    completed index

--- clb_web-design/js/js.js
+++ clb_web-design/js/js.js
@@ -1,36 +1,27 @@
 var i = 0;
 var pos = 0;
 var size = 0;
-var menuStatus = 0;
 
 jQuery(document).ready(function () {
-  size = screen.width;
+  size = $(document).width();
   $(window).resize(function () {
     size = $(window).width();
-    if (menuStatus === 0) {
-      $(".menu-list").css("opacity", "0");
-      $(".menu-list").css("visibility", "hidden");
+    if (size > 1100) {
+      $(".menu").css("background-color", "var(--transparent)");
+      $(".menu").css("border-bottom", "3px solid var(--border)");
+      $(".menu").css("border-right", "none");
+      $(".menu-item-txt").css("color", "var(--txt-color)");
     } else {
-      if (size > 1100) {
-        $(".menu").css("background-color", "var(--transparent)");
-        $(".menu").css("border-bottom", "3px solid var(--border)");
-        $(".menu").css("border-right", "none");
+      if (pos < 500) {
+        $(".menu").css("background-color", "var(--txt-color2)");
+        $(".menu").css("border-bottom", "none");
+        $(".menu").css("border-right", "3px solid var(--border)");
         $(".menu-item-txt").css("color", "var(--txt-color)");
-        menuStatus = 1;
       } else {
-        if (pos < 500) {
-          $(".menu").css("background-color", "var(--txt-color2)");
-          $(".menu").css("border-bottom", "none");
-          $(".menu").css("border-right", "3px solid var(--border)");
-          $(".menu-item-txt").css("color", "var(--txt-color)");
-          menuStatus = 1;
-        } else {
-          $(".menu").css("background-color", "var(--transparent)");
-          $(".menu").css("border-bottom", "none");
-          $(".menu").css("border-right", "3px solid var(--border)");
-          $(".menu-item-txt").css("color", "var(--txt-color2)");
-          menuStatus = 1;
-        }
+        $(".menu").css("background-color", "var(--transparent)");
+        $(".menu").css("border-bottom", "none");
+        $(".menu").css("border-right", "3px solid var(--border)");
+        $(".menu-item-txt").css("color", "var(--txt-color2)");
       }
     }
   });
@@ -38,15 +29,72 @@ jQuery(document).ready(function () {
   $(window).scroll(function () {
     pos = $("html").scrollTop();
 
-    if (pos < 500) {
-      $(".scrollTop").css("display", "none");
+    if ((pos < 500 && size > 1100) || (pos > 500 && size > 1100)) {
+      $(".menu").css("background-color", "var(--transparent)");
+      $(".menu").css("border-bottom", "3px solid var(--border)");
+      $(".menu").css("border-right", "none");
+      $(".menu-item-txt").css("color", "var(--txt-color)");
+
+      $(".menu-list").css("opacity", "1");
+      $(".menu-list").css("visibility", "visible");
+      $(".menu-box-scroll").css("transform", "rotate(0deg)");
+      $(".menu-box-scroll").css("animation", "rotate2 0.3s linear");
+      $(".menu-list").css("transition", "0.5s");
+    } else if (pos < 500 && size <= 1100) {
+      $(".menu").css("background-color", "var(--txt-color2)");
+      $(".menu").css("border-bottom", "none");
+      $(".menu").css("border-right", "3px solid var(--border)");
+      $(".menu-item-txt").css("color", "var(--txt-color)");
+
+      $(".menu-list").css("opacity", "1");
+      $(".menu-list").css("visibility", "visible");
+      $(".menu-box-scroll").css("transform", "rotate(0deg)");
+      $(".menu-box-scroll").css("animation", "rotate2 0.3s linear");
+      $(".menu-list").css("transition", "0.5s");
     } else {
-      $(".scrollTop").css("display", "block");
+      $(".menu").css("background-color", "var(--transparent)");
+      $(".menu").css("border-bottom", "none");
+      $(".menu").css("border-right", "3px solid var(--border)");
+      $(".menu-item-txt").css("color", "var(--txt-color2)");
+
+      $(".menu-list").css("opacity", "1");
+      $(".menu-list").css("visibility", "visible");
+      $(".menu").css("background-color", "var(--transparent)");
+      $(".menu-box-scroll").css("transform", "rotate(0deg)");
+      $(".menu-box-scroll").css("animation", "rotate2 0.3s linear");
+      $(".menu-list").css("transition", "0.5s");
     }
+
+    $(".menu-list").css("opacity", "0");
+    $(".menu-list").css("visibility", "hidden");
+    $(".menu").css("border-bottom", "none");
+    $(".menu").css("border-right", "none");
+    $(".menu").css("background-color", "transparent");
+    $(".menu-box-scroll").css("animation", "rotate1 0.3s linear");
+    $(".menu-list").css("transition", "0.5s");
+
+    i++;
+
+    // if (pos < 500) {
+    //   $(".scrollTop").css("display", "none");
+    //   $(".menu").css("background-color", "var(--txt-color2)");
+    //   $(".menu").css("border-bottom", "none");
+    //   $(".menu").css("border-right", "3px solid var(--border)");
+    //   $(".menu-item-txt").css("color", "var(--txt-color)");
+    // } else {
+    //   $(".scrollTop").css("display", "block");
+    //   $(".menu").css("background-color", "var(--transparent)");
+    //   $(".menu").css("border-bottom", "none");
+    //   $(".menu").css("border-right", "3px solid var(--border)");
+    //   $(".menu-item-txt").css("color", "var(--txt-color2)");
+    // }
   });
 
   $(".scrollTop").click(function () {
-    $("html").scrollTop(0);
+    $("html").animate({scrollTop: 0}, 500);
+  });
+  $(".getstart-btn").click(function () {
+    $("html").animate({scrollTop: 800}, 500);
   });
 
   $(".menu-box-scroll").click(function () {
@@ -58,7 +106,6 @@ jQuery(document).ready(function () {
       $(".menu").css("background-color", "transparent");
       $(".menu-box-scroll").css("animation", "rotate1 0.3s linear");
       $(".menu-list").css("transition", "0.5s");
-      menuStatus = 0;
 
       i++;
     } else {
@@ -74,7 +121,6 @@ jQuery(document).ready(function () {
         $(".menu-box-scroll").css("animation", "rotate2 0.3s linear");
         $(".menu-list").css("transition", "0.5s");
         i++;
-        menuStatus = 1;
       } else if (pos < 500 && size <= 1100) {
         $(".menu").css("background-color", "var(--txt-color2)");
         $(".menu").css("border-bottom", "none");
@@ -87,7 +133,6 @@ jQuery(document).ready(function () {
         $(".menu-box-scroll").css("animation", "rotate2 0.3s linear");
         $(".menu-list").css("transition", "0.5s");
         i++;
-        menuStatus = 1;
       } else {
         $(".menu").css("background-color", "var(--transparent)");
         $(".menu").css("border-bottom", "none");
@@ -101,7 +146,6 @@ jQuery(document).ready(function () {
         $(".menu-box-scroll").css("animation", "rotate2 0.3s linear");
         $(".menu-list").css("transition", "0.5s");
         i++;
-        menuStatus = 1;
       }
     }
   });
